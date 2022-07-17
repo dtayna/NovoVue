a2c45c62ba999882090ffa56b7552b7f7f169949
Débora Tayná <51178605+dtayna@users.noreply.github.com>
2022-07-16 21:52:15 -0300

    Initial commit

---
+++ src/components/Form.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <label> Número de Tabs: </label>
+    <button @click="increment()">{{ count }}</button>
+    <br />
+    <br />
+    <div v-for="(a, i) in quant" :key="i">
+      <label> Titulo: </label>
+      <input @change="addTitulo(i)" />
+      <br />
+      <label> Conteúdo: </label>
+      <textarea @change="addConteudo(i, this.value)"> </textarea>
+    </div>
+    <button @click="submeter()">Submeter</button>
+  </div>
+  <div v-if="nivel === 1">
+    <Tab :topos="coisa1" :corpos="coisa2" />
+  </div>
+</template>
+
+<script>
+import Tab from "./Tab.vue";
+
+export default {
+  components: {
+    Tab: Tab,
+  },
+  data() {
+    return {
+      count: 0,
+      quant: [],
+      coisa1: [],
+      coisa2: [],
+      nivel: 0,
+      teste: 0,
+      titulos: ["a"],
+      conteudos: [],
+    };
+  },
+  methods: {
+    increment() {
+      this.quant[this.count] = this.count;
+      this.count++;
+    },
+    addConteudo(j, comp) {
+      this.conteudos[j] = comp.value;
+    },
+    addTitulo(j, comp) {
+      this.teste = 1;
+      this.titulos[j] = "ai" + comp;
+    },
+    submeter() {
+      this.nivel = 1;
+      this.coisa1 = this.titulo;
+      this.coisa2 = this.conteudos;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Tab.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="Tab">
+    <div class="botoes" v-for="(item, i) in topos" :key="i">
+      <button @click="selecionaTab(i)">{{ item }}</button>
+    </div>
+    <br />
+    <div class="corpo">
+      <p>{{ corpos[tabS] }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    topos: [],
+    corpos: [],
+  },
+
+  data() {
+    return {
+      count: 0,
+      tabS: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    selecionaTab(id) {
+      this.tabS = id;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.botoes {
+  margin: 10px;
+  width: 50px;
+  float: left;
+}
+
+.corpo {
+  width: 100px;
+}
+</style>
